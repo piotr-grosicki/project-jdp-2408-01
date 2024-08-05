c7af474f9f9c7cc6313109c5cf212b31f07895d1
SantyJustyna <justyna_santy@hotmail.com>
2024-08-05 13:38:44 +0200

    ProductController

---
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.kodilla.ecommercee.controller;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/v1/products")
+public class ProductController {
+
+    @GetMapping
+    public List<ProductDto> getProducts() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = "{productId}")
+    public ProductDto getProduct(Long productId) {
+        return new ProductDto(1L, "productName", 3, new BigDecimal(12));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createProduct(ProductDto productDto) {
+
+    }
+
+    @PutMapping(value = "{productId}")
+    public ProductDto updateProduct(ProductDto productDto) {
+        return new ProductDto(1L, "productName", 3, new BigDecimal(12));
+    }
+
+    @DeleteMapping
+    public void deleteProduct(Long productId) {
+
+    }
+}
