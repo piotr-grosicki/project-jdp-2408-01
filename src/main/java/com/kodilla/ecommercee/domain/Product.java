dbc40605cc03c9d51163af127f8cf2383321ef22
SantyJustyna <justyna_santy@hotmail.com>
2024-08-06 18:44:42 +0200

    Product & ProductRepository

---
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -0,0 +1,56 @@
+package com.kodilla.ecommercee.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+
+
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity(name = "products")
+public class Product {
+    @Id
+    @NotNull
+    @GeneratedValue
+    private Long productId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "groupId")
+    private Group group;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "join_carts_products",
+            joinColumns = {@JoinColumn(name = "productId", referencedColumnName = "productId")},
+            inverseJoinColumns = {@JoinColumn(name = "cartId", referencedColumnName = "cartId")}
+    )
+    private List<Cart> carts = new ArrayList<>();
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "join_orders_products",
+            joinColumns = {@JoinColumn(name = "productId", referencedColumnName = "productId")},
+            inverseJoinColumns = {@JoinColumn(name = "orderId", referencedColumnName = "orderId")}
+    )
+    private List<Order> orders = new ArrayList<>();
+
+}
